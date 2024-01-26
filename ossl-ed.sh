b7257ce5c5878c64f9124906ba9aa004b027e28b
jroovyboi <33566635+jroovy@users.noreply.github.com>
2024-01-26 11:21:27 +0700

    replace #!/bin/bash with #!/usr/bin/env bash
    fixes issues for systems that don't have a /bin folder

--- ossl-ed.sh
+++ ossl-ed.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 sslPath='openssl'
 tmpDir='/tmp'
--- ossl-gen.sh
+++ ossl-gen.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 sslPath='openssl'
 centralPassDir="${HOME}/.config/ossl-ed"
